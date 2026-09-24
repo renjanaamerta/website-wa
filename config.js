f366c49e68877657762d0f6bc604e21ac53ab290
renjanaamerta <renjanaamerta72@gmail.com>
2026-09-24 10:40:40 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RENJANA_CONFIG={
+  SUPABASE_URL:"https://hgqxxkptjiaivlkpydqo.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY:"sb_publishable_S_PqfQ1xGR7902irCshdmg_eIeoxaIC",
+  WHATSAPP_NUMBER:"6281998714500"
+};
